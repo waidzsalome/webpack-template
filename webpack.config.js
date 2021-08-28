6c9944dcabaa5cb036c15dfb4553d4a9b67885a4
韩薇 <hanwei@172-15-18-247.lightspeed.brhmal.sbcglobal.net>
2021-08-28 16:26:06 +0800

    init

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: {
+    index: './src/index.js',
+  },
+  devtool: 'inline-source-map',
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'dist'),
+    },
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'development',
+    }),
+  ],
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  }
+}
