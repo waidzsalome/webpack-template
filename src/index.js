d16120a7f938732e87c590d1b53511623c955392
hanwei <hanwei1@songguo7.com>
2021-08-31 20:23:47 +0800

    output management

--- src/index.js
+++ src/index.js
@@ -1,31 +1,16 @@
 import _ from 'lodash';
-import './style.css';
-import Icon from "./test.png";
-import Data from "./data.xml";
-import Notes from "./data.csv";
-import toml from './data.toml';
-import yaml from './data.yaml';
-import json from "./data.json5";
-
-console.log(toml.title);
-console.log(toml.owner.name);
-console.log(yaml.title);
-console.log(yaml.owner.name);
-console.log(json.title);
-console.log(json.owner.name);
+import printMe from "./print";
 
 function component() {
   const element = document.createElement('div');
+  const btn = document.createElement('button');
 
   // lodash（目前通过一个 script 引入）对于执行这一行是必需的
   // lodash 在当前 script 中使用 import 引入
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  element.classList.add('hello');
-  const myIcon = new Image();
-  myIcon.src = Icon;
-  element.appendChild(myIcon);
-  console.log(Data);
-  console.log(Notes);
+  btn.innerHTML = 'Click me and check the console!';
+  btn.onclick = printMe;
+  element.appendChild(btn);
   return element;
 }
 
--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,10 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const toml = require('toml');
-const yaml = require('yamljs');
-const json5 = require('json5');
 
 module.exports = {
-  mode: 'development',
   entry: {
     index: './src/index.js',
+    print: './src/print.js',
   },
   devtool: 'inline-source-map',
   devServer: {
@@ -15,44 +12,9 @@ module.exports = {
       directory: path.join(__dirname, 'dist'),
     },
   },
-  module: {
-    rules: [
-      {
-        test: /\.css$/i,
-        use: ['style-loader', 'css-loader'],
-      }, {
-        test: /\.(png|svg|jpg|jpeg|gif)$/i,
-        type: 'asset/resource',
-      }, {
-        test: /\.(csv|tsv)$/i,
-        use: ['csv-loader'],
-      }, {
-        test: /\.xml$/i,
-        use: ['xml-loader'],
-      }, {
-        test: /\.toml$/i,
-        type: 'json',
-        parser: {
-          parse: toml.parse,
-        },
-      }, {
-        test: /\.yaml$/i,
-        type: 'json',
-        parser: {
-          parse: yaml.parse,
-        },
-      }, {
-        test: /\.json5$/i,
-        type: 'json',
-        parser: {
-          parse: json5.parse,
-        },
-      }
-    ]
-  },
   plugins: [
     new HtmlWebpackPlugin({
-      title: 'development',
+      title: 'output Management',
     }),
   ],
   output: {
