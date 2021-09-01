8cb6140b1265b8fae1f4040c08962293147dd4b9
hanwei <hanwei1@songguo7.com>
2021-09-01 11:45:05 +0800

    HMRbasic

--- webpack.config.js
+++ webpack.config.js
@@ -1,27 +1,32 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require('webpack');
 
 module.exports = {
   mode: 'development',
   entry: {
-    index: './src/index.js',
-    print: './src/print.js',
+    app: './src/index.js',
+    // hot: 'webpack/hot/dev-server.js',
+    // client: 'webpack-dev-server/client/index.js?hot=true&live-reload=true',
   },
   devtool: 'inline-source-map',
   devServer: {
     static: {
       directory: path.join(__dirname, 'dist'),
     },
+    hot: true,
+    // hot: false,
+    // client: false,
   },
   plugins: [
     new HtmlWebpackPlugin({
       title: 'development',
     }),
+    // new webpack.HotModuleReplacementPlugin(),
   ],
   output: {
     filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist'),
     clean: true,
-    publicPath: '/',
   }
 }
