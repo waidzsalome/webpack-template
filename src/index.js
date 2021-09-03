bf9dd7331685ae4ef511536a46a00d17e9b1745a
hanwei <hanwei1@songguo7.com>
2021-09-03 11:45:58 +0800

    web workers

--- src/index.js
+++ src/index.js
@@ -1,13 +1,8 @@
-import _ from 'lodash';
-
-function component() {
-  const element = document.createElement('div');
-
-  // lodash（目前通过一个 script 引入）对于执行这一行是必需的
-  // lodash 在当前 script 中使用 import 引入
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-
-  return element;
-}
-
-document.body.appendChild(component());
+const worker = new Worker(new URL('./deep-thought.js', import.meta.url));
+console.log(worker)
+worker.postMessage({
+  question: "The Answer to the Ultimate Question of Life, The Universe, and Everything."
+});
+worker.onmessage = ({ data: { answer } }) => {
+  console.log(answer);
+};
