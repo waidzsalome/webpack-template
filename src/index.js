14aba3330b6a1535f93ed5c767ffae96ca993cb0
hanwei <hanwei1@songguo7.com>
2021-09-06 21:08:18 +0800

    public Path Examples

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import _ from 'lodash';
-
+console.log(process.env.ASSET_PATH);
 function component() {
   const element = document.createElement('div');
-
+  console.log(process.env.NODE_ENV);
   // lodash（目前通过一个 script 引入）对于执行这一行是必需的
   // lodash 在当前 script 中使用 import 引入
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,10 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-
+const webpack = require('webpack');
+// console.log(process.env);
+// __webpack_public_path__ = process.env.ASSET_PATH;
+// console.log(__webpack_public_path__);
+const ASSET_PATH = process.env.ASSET_PATH || '/';
 module.exports = {
   mode: 'development',
   entry: {
@@ -16,10 +20,14 @@ module.exports = {
     new HtmlWebpackPlugin({
       title: 'development',
     }),
+    new webpack.DefinePlugin({
+      'process.env.ASSET_PATH': JSON.stringify(ASSET_PATH),
+    }),
   ],
   output: {
     filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist'),
     clean: true,
+    // publicPath: ASSET_PATH,
   }
 }
